e2b7cde511b4af78b9f7d4cd87a9f36d72f0dc58
BNdungu <bryannganga909@gmail.com>
2023-12-25 22:09:10 +0300

    app

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const postRoter = require('./routes/postRoutes')
+const {MONGO_IP,MONGO_PASSWORD,MONGO_PORT,MONGO_USER} = require('./config/config')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const connectWithRetry = () => {
+  mongoose
+  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authMechanism=DEFAULT`)
+    .then(() => console.log('Connected to the DB successfully'))
+    .catch((error) => {
+      console.log(error)
+      setTimeout(connectWithRetry, 5000)
+    }
+    )
+}
+
+app.get('/', (req,res) => {
+    res.send('Hello world! my name is Nganga Ndungu')
+})
+
+app.get('/docker',(req,res) => {
+  res.send('Hello Docker ')  
+})
+
+app.listen(port, () => {
+    console.log(`Server started  to listening at port ${port}`)
+})
+
+connectWithRetry()
