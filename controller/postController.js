601456bc8d947b2e4376f3fd9907a82c904e8cab
BNdungu <bryannganga909@gmail.com>
2023-12-25 21:45:02 +0300

    -

--- controller/postController.js
+++ controller/postController.js
@@ -4,7 +4,17 @@ const Post = require('../models/postModel')
 exports.getAllPosts = async (req,res,next) => {
     try {
         const posts = await Post.find()
+
+        res.status(200).json({
+            status: 'success',
+            results: posts.length,
+            data: {
+                posts
+            }
+        })
     } catch (error) {
-        
+        res.status(400).json({
+            status: 'fail'
+        })
     }
 }
