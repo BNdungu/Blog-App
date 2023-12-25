54c4ab8f3f2f2f3d792fbc927f33df0241242ba3
BNdungu <bryannganga909@gmail.com>
2023-12-25 21:42:28 +0300

    schema

---
+++ controller/postController.js
@@ -0,0 +1,10 @@
+const Post = require('../models/postModel')
+
+
+exports.getAllPosts = async (req,res,next) => {
+    try {
+        const posts = await Post.find()
+    } catch (error) {
+        
+    }
+}
---
+++ models/postModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: [true, "Post must have a title"]
+    },
+
+    body:{
+        type: String,
+        require: [true, 'Post must have body']
+    }
+})
+
+module.exports = mongoose.model('Post', postSchema)
