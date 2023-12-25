8a28aef76023fc4b99e27532e97f0f6c7e47816a
BNdungu <bryannganga909@gmail.com>
2023-12-25 23:27:48 +0300

    *

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,16 @@
 const user = require ('../models/userModel')
+const bcrypt = require('bcryptjs')
 
 exports.signUp = async (req,res) => {
+    const {username, password} = req.body
+    const hashPassword = await bcrypt.hash(password,12)
+
     try {
-       const newUser = await user.create(req.body)
+       const newUser = await user.create({
+            username,
+            password: hashPassword
+            })
+
        res.status(201).json({
         status: 'success',
         data : {
